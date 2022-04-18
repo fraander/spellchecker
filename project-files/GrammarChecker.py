65fcdf6f4e982b2ec20ddab117ad9941ebecae2b
fraander <57777918+fraander@users.noreply.github.com>
2022-04-18 11:44:26 -0400

    refactor speed error fixed--new issue: not finding contractions

--- project-files/GrammarChecker.py
+++ project-files/GrammarChecker.py
@@ -37,15 +37,19 @@ class GrammarChecker:
                 print("Invalid input. Try again.")
 
     def __init__(self):
-        dictionary = Dictionary().data  # dictionary
-        filename = "doc_wrong.txt" # FileManager().fn  # file manager
+        dictionary = Dictionary()  # dictionary
+        filename = "doc_wrong.txt"  # FileManager().fn  # file manager
 
         with open("edited+" + filename, 'w') as o:
             with open(filename, 'r') as f:  # open read and write files
                 buffer = ""  # create a buffer so that the program is not limited in memory and can read large files
+                capitalize = False
 
                 while True:
                     c = f.read(1)  # read by character
+
+                    if len(buffer) > 1 and buffer[0] in string.ascii_uppercase:
+                        capitalize = True
 
                     if not c:  # if eof, break
                         # TODO: do something at eof since there are eof errors right now
@@ -55,11 +59,11 @@ class GrammarChecker:
                     if c not in string.punctuation and c not in string.whitespace and buffer[:-1] + c != "\n":
                         buffer += c  # increase the buffer
                     else:  # if there is whitespace (new word), access the buffer
-                        w = Dictionary().clean_input(buffer)  # clean the buffer for easy searching
-                        word_in = Dictionary().word_in(w)  # check for the word in the dictionary
+                        w = dictionary.clean_input(buffer)  # clean the buffer for easy searching
+                        word_in = dictionary.word_in(w)  # check for the word in the dictionary
 
                         if not word_in:  # if the word was not found
-                            options = Dictionary().find_similar(w)  # find similar words
+                            options = dictionary.find_similar(w)  # find similar words
                             GrammarChecker.print_options(options, buffer)  # print options
                             buffer = GrammarChecker.get_choice(options, w)  # get choice from user
 
@@ -69,10 +73,14 @@ class GrammarChecker:
                         elif buffer[:-1] + c == "\t":  # if a tab, change c to tab
                             c = "\t"
 
+                        if capitalize:
+                            buffer = buffer.capitalize()
+
                         o.write(buffer + c)  # write to new file
                         print(buffer + c, end="")  # output for user to see
 
                         buffer = ""  # when done, clear the buffer
+                        capitalize = False
 
     @staticmethod
     def get_custom(w):
--- project-files/main.py
+++ project-files/main.py
@@ -1,7 +1,6 @@
 import string
 
 from GrammarChecker import GrammarChecker
-from Dictionary import Dictionary
 
 # Turn on Soft Wrapping for the Console or else the outputs will be hard to read.
 # View -> Active Editor -> Soft Wrap
